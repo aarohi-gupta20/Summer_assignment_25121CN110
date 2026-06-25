497991fe47ac6888d6ee356f1ea61742067dc005
Aarohi gupta <madhavpari1232@gmail.com>
2026-06-25 22:00:42 +0530

    Add files via upload

---
+++ Day 25/100_sort_words_by_length.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of words: ";
+    cin >> n;
+
+    string words[n];
+
+    // Input words
+    cout << "Enter words:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+    }
+
+    // Sort words by length
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (words[i].length() > words[j].length()) {
+                string temp = words[i];
+                words[i] = words[j];
+                words[j] = temp;
+            }
+        }
+    }
+
+    // Display sorted words
+    cout << "\nWords sorted by length:\n";
+    for (int i = 0; i < n; i++) {
+        cout << words[i] << " ";
+    }
+
+    return 0;
+}
